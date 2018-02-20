b0c15a36cb2a000b21f00dcae0946778a39a022c
Lukas Rist <lr@cortilabs.com>
2018-02-20 08:50:44 +0100

    write, read and bench

--- wal.go
+++ wal.go
@@ -1,6 +1,29 @@
 package wal
 
-import "hash/crc32"
+import (
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
+	"io"
+)
+
+const (
+	// FULL record
+	FULL = iota + 1
+	// FIRST part of a record
+	FIRST
+	// MIDDLE part of a record
+	MIDDLE
+	// LAST part of a record
+	LAST
+)
+
+// WAL is a write ahead log
+type WAL struct {
+	buffer    bytes.Buffer
+	pos       int
+	blockSize int
+}
 
 // Record contains a log message
 type Record struct {
@@ -10,10 +33,93 @@ type Record struct {
 	Data     []uint8
 }
 
+// NewWAL create a new write ahead log
+func NewWAL() WAL {
+	return WAL{
+		blockSize: 2 << 15, // 32kb
+	}
+}
+
+// NewRecord creates a new WAL Record
+func NewRecord(data []byte) Record {
+	record := Record{Data: data}
+	record.len()
+	record.crc()
+	return record
+}
+
 func (r *Record) crc() {
 	r.Checksum = crc32.ChecksumIEEE(append(r.Data, r.Type))
 }
 
 func (r *Record) len() {
 	r.Length = uint16(len(r.Data))
+}
+
+// Bytes returns the record in bytes
+func (r *Record) Bytes() ([]byte, error) {
+	buffer := bytes.Buffer{}
+	if err := binary.Write(&buffer, binary.BigEndian, r.Checksum); err != nil {
+		return []byte{}, err
+	}
+	if err := binary.Write(&buffer, binary.BigEndian, r.Length); err != nil {
+		return []byte{}, err
+	}
+	if err := binary.Write(&buffer, binary.BigEndian, r.Type); err != nil {
+		return []byte{}, err
+	}
+	if err := binary.Write(&buffer, binary.BigEndian, r.Data); err != nil {
+		return []byte{}, err
+	}
+	return buffer.Bytes(), nil
+}
+
+func (wal *WAL) padBlock(spaceInBlock int) error {
+	padding := make([]byte, spaceInBlock)
+	n, err := wal.buffer.Write(padding)
+	if err != nil {
+		return err
+	}
+	wal.pos += n
+	return nil
+}
+
+func (wal *WAL) Write(record *Record) error {
+	spaceInBlock := wal.blockSize - (wal.pos % wal.blockSize)
+	if spaceInBlock < 7 {
+		wal.padBlock(spaceInBlock)
+		spaceInBlock = wal.blockSize
+	}
+	data, err := record.Bytes()
+	if err != nil {
+		return err
+	}
+	if len(data)+7 > spaceInBlock { // body = len(data) + header
+		record.Type = FIRST
+	}
+	n, err := wal.buffer.Write(data)
+	if err != nil {
+		return err
+	}
+	wal.pos += n
+	return nil
+}
+
+func (wal *WAL) Read(r io.Reader) (Record, error) {
+	record := Record{}
+	if err := binary.Read(r, binary.BigEndian, &record.Checksum); err != nil {
+		return record, err
+	}
+	if err := binary.Read(r, binary.BigEndian, &record.Length); err != nil {
+		return record, err
+	}
+	if err := binary.Read(r, binary.BigEndian, &record.Type); err != nil {
+		return record, err
+	}
+	data := make([]byte, record.Length)
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return record, err
+	}
+	record.Data = data
+	return record, nil
 }
--- wal_test.go
+++ wal_test.go
@@ -1,11 +1,12 @@
 package wal
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestCRC(t *testing.T) {
-	r := Record{Type: 1, Data: []uint8{1}}
+	r := Record{Type: FULL, Data: []uint8{1}}
 	r.crc()
 	if r.Checksum != 801444648 {
 		t.Fatalf("Expecting checksum %d, got %d", 801444648, r.Checksum)
@@ -17,5 +18,74 @@ func TestLen(t *testing.T) {
 	r.len()
 	if r.Length != 1 {
 		t.Fatalf("Expecting length %d, got %d", 1, r.Length)
+	}
+}
+
+func TestWrite(t *testing.T) {
+	r := Record{Data: []uint8{1}}
+	wal := NewWAL()
+	if err := wal.Write(&r); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRead(t *testing.T) {
+	wal := NewWAL()
+	record := Record{Type: FULL, Data: []byte{1, 2, 3, 4, 5}}
+	record.len()
+	record.crc()
+	data, err := record.Bytes()
+	if err != nil {
+		t.Error(err)
+	}
+	r := bytes.NewReader(data)
+	_, err = wal.Read(r)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPadding(t *testing.T) {
+	wal := NewWAL()
+	wal.padBlock(1)
+	if wal.pos != 1 {
+		t.Errorf("Expected wal.pos %d got %d", 1, wal.pos)
+	}
+}
+
+func TestFullBlock(t *testing.T) {
+	wal := NewWAL()
+	wal.blockSize = 10
+	wal.pos = 5
+	record := NewRecord([]byte{1, 2, 3})
+	wal.Write(&record)
+}
+
+func BenchmarkWrite(b *testing.B) {
+	r := Record{Data: []uint8{1}}
+	wal := NewWAL()
+	for n := 0; n < b.N; n++ {
+		if err := wal.Write(&r); err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkRead(b *testing.B) {
+	wal := NewWAL()
+	record := Record{Type: FULL, Data: []byte{1, 2, 3, 4, 5}}
+	record.len()
+	record.crc()
+	data, err := record.Bytes()
+	if err != nil {
+		b.Error(err)
+	}
+	r := bytes.NewReader(data)
+	r.Seek(0, 0)
+	for n := 0; n < b.N; n++ {
+		if _, err := wal.Read(r); err != nil {
+			b.Error(err)
+		}
+		r.Seek(0, 0)
 	}
 }
